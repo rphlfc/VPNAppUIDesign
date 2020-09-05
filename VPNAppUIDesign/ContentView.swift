1faa870d228447d32c97fcedc53eb229d81ff02e
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-05 19:33:22 -0300

    Initial Commit

---
+++ VPNAppUIDesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VPNAppUIDesign
+//
+//  Created by Raphael Cerqueira on 05/09/20.
+//  Copyright © 2020 Raphael Cerqueira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
